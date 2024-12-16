a29ae7a59f26108ef83d0aaa574069626737aff9
Thoybur-Rohman <Tayyb.r17@yahoo.com>
2024-12-16 11:26:25 +0000

    added product and category Entities and DTOs

---
+++ eCommerceApp.Application/DTOs/Category/CategoryBase.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerceApp.Application.DTOs.Category
+{
+    public class CategoryBase
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}

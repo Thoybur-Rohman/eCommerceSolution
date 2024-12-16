d1a718a256814d29072389d089b129e295433324
Thoybur-Rohman <Tayyb.r17@yahoo.com>
2024-12-16 14:00:37 +0000

    added repo and interface

---
+++ eCommerceApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerceApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceApp.Infrastructure.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ eCommerceApp.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using eCommerceApp.Domain.Interfaces;
+using eCommerceApp.Infrastructure.Data;
+
+namespace eCommerceApp.Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity>(AppDbContext context) : IGeneric<TEntity> where TEntity : class
+    {
+        public Task<int> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

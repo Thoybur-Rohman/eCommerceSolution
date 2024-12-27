59bf0fb9c08a3d94f45869b04cefe11fadd2e05a
Thoybur-Rohman <Tayyb.r17@yahoo.com>
2024-12-27 11:12:56 +0000

    added category controller

---
+++ eCommerceApp.Host/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using eCommerceApp.Application.DTOs.Category;
+using eCommerceApp.Application.DTOs.Product;
+using eCommerceApp.Application.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerceApp.Host.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController(ICategoryService categoryService) : ControllerBase
+    {
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await categoryService.GetAllAsync();
+            return data.Any() ? Ok(data) : NotFound(data);
+        }
+
+        [HttpGet("single/{id}")]
+        public async Task<IActionResult> GetSingle(Guid id)
+        {
+            var data = await categoryService.GetByIdAsync(id);
+            return data != null ? Ok(data) : NotFound(data);
+        }
+
+        [HttpPost("add")]
+        public async Task<IActionResult> Add([FromBody] CreateCategory createCategory)
+        {
+            var result = await categoryService.AddAsync(createCategory);
+            return result.Success ? StatusCode(201, result) : BadRequest(result);
+        }
+        [HttpPost("update")]
+        public async Task<IActionResult> Add(UpdateCategory updateCategory)
+        {
+            var result = await categoryService.UpdateAsync(updateCategory);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPost("delete/{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var result = await categoryService.DeleteAsync(id);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+    }
+}
+

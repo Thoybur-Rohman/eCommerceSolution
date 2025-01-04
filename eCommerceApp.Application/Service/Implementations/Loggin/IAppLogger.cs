d34cb0b28d3d15d11f25fd67fcb866fee376d59d
Thoybur-Rohman <Tayyb.r17@yahoo.com>
2025-01-04 12:23:42 +0000

    added logger imp

---
+++ eCommerceApp.Application/Service/Implementations/Loggin/IAppLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Application.Service.Implementations.Login
+{
+    public interface IAppLogger<T>
+    {
+        void LogInformation(string message);
+        void LogError(Exception ex, string message);
+        void LogWarning(string message);
+    }
+}
---
+++ eCommerceApp.Infrastructure/Service/SeriLogLoggerAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eCommerceApp.Application.Service.Implementations.Login;
+using Microsoft.Extensions.Logging;
+
+namespace eCommerceApp.Infrastructure.Service
+{
+    public class SerilogLoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public SerilogLoggerAdapter(ILogger<T> logger) // Constructor 
+        {
+            _logger = logger;
+        }
+
+        public void LogError(Exception ex, string message) => _logger.LogError(ex, message);
+        public void LogInformation(string message) => _logger.LogInformation(message);
+        public void LogWarning(string message) => _logger.LogWarning(message);
+    }
+}
